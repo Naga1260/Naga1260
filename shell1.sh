8d3b4f3e0c2f381b7c5ff4ca59cfc0e2262df558
Naga1260 <tulanagarjuna@gmail.com>
2021-10-20 10:55:54 +0800

    Update the script with colors

--- shell1.sh
+++ shell1.sh
@@ -2,3 +2,9 @@
 echo "myworld"
 ls -lrt
 printf "DevOps learning"
+echo -e "\e[31mtext is in red\e[0m"
+echo -e "\e[32mtext is in green\e[0m"
+echo -e "\e[33mtext is in yellow\e[0m"
+echo -e "\e[34mtext is in blue\e[0m"
+echo -e "\e[35mtext is in magneta\e[0m"
+echo -e "\e[36mtext is in cyan\e\0m"
