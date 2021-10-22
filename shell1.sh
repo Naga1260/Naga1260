5731e59b08d186d62a5b28576f42bb4e8ab37242
Naga1260 <tulanagarjuna@gmail.com>
2021-10-22 19:58:41 +0800

    My first function

--- shell1.sh
+++ shell1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+myfunc() {
 echo "myworld"
 ls -lrt
 printf "DevOps learning\n"
@@ -8,3 +9,7 @@ echo -e "\e[33mtext is in yellow\e[0m"
 echo -e "\e[34mtext is in blue\e[0m"
 echo -e "\e[35mtext is in magneta\e[0m"
 echo -e "\e[36mtext is in cyan\e\0m"
+}
+
+echo -e "Here is the funtion call"
+myfunc()
